eb9ba39c233b8a5ad4699fe0881edab2b5a2c318
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-24 09:59:49 +0500

    devfolio updated with href

--- devfolio/flutter_service_worker.js
+++ devfolio/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a29e8b22a08aa0274b027931eec216c4",
-"index.html": "21576ac337a41ed6d9ac033b1db46bc6",
-"/": "21576ac337a41ed6d9ac033b1db46bc6",
+"index.html": "c6c8e87157c55fb1783e60cfd5248eb0",
+"/": "c6c8e87157c55fb1783e60cfd5248eb0",
 "main.dart.js": "bf4d53e3c417b2f73dbbcb154a019132",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "61b493df9635f221d909421e8769acb0",
